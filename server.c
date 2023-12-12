236500aeca971fa066ce1c4cbe750f89b7d3923e
Youssefch05 <youssefchaoui21@gmail.com>
2023-12-12 14:20:24 +0100

    fonc

--- server.c
+++ server.c
@@ -41,41 +41,125 @@ return 0;
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <stdbool.h>
 #define  AJOUT 0
 #define RETRAIT 1
 #define SOLDE 2
-#define OPERATION 3
-
-typedef struct {
-   int identifiant;
-char password[256];
-int nb_comptes;
-int id_compte[nb_comptes];
+#define DERNIERES_OPERATION 3
 
 
-
+typedef struct Date{
+   int jour;
+   int mois;
+   int annee;
+}date_t;
 
 
 
-}client_t;
+typedef struct{
+   int montant;
+   int type_operation;
+   date_t date_operation;
+}operation_t;
+
+
 
 typedef struct{
    int solde;
    int id_compte;
-   int derniere_operation[10];
+   operation_t dernieres_operations[10];
 
 }compte_t;
 
+typedef struct {
+   int identifiant;
+char password[256];
+   int id_compte[2];
+compte_t compte[2];
+
+
+}client_t;
+
+
+
+typedef struct{
+   client_t clients[5];
+   compte_t comptes[10];
+} banque_t;
 compte_t tab_compte[10];
 
 client_t tab_client[5];
 
 
 
+int find_client(int id){
+   for(int i = 0; i<5; i++){
+      if(tab_client[i].identifiant == id){
+         return 1; //1 si le client existe
+      }
+   }
+   return 0; //0 si le client n'existe pas
+}
+
+int correctpassword(client_t client, char *password){
+   if(strcmp(client.password, password) == 0){
+      return 1; //1 si le mot de passe est correct
+   }
+   return 0; //0 si le mot de passe est incorrect
+}
+
+int find_compte(client_t client, int id){
+   for(int i = 0; i<2; i++){
+      if(client.id_compte[i] == id){
+         return 1; //1 si le compte est associé au client
+      }
+   }
+   return 0; //0 si le compte n'est pas associé au client
+}
+          
+void initializeBanque(banque_t *banque) {
+    for (int i = 0; i < 5; i++) {
+        banque->clients[i].identifiant = i + 1;
+         printf("banque->clients[%d].identifiant = %d\n", i, banque->clients[i].identifiant);
+        sprintf(banque->clients[i].password, "password%d", i + 1);
+        printf("banque->clients[%d].password = %s\n", i, banque->clients[i].password);
+         banque->clients[i].id_compte[0] = i * 2 + 1;
+         banque->clients[i].id_compte[1] = i * 2 + 2;//2 comptes par client
+         printf("banque->clients[%d].id_compte[0] = %d\n", i, banque->clients[i].id_compte[0]);
+         printf("banque->clients[%d].id_compte[1] = %d\n", i, banque->clients[i].id_compte[1]);
+         
+
+
+
+        
+
+        for (int j = 0; j < 2; j++) {
+            banque->clients[i].compte[j].solde = (i + 1) * 1000 + j * 500;
+            banque->clients[i].compte[j].id_compte = i * 2 + j + 1;
+            printf("banque->clients[%d].compte[%d].solde = %d\n", i, j, banque->clients[i].compte[j].solde);
+
+            // Initialize last 10 operations for each account
+            for (int k = 0; k < 10; k++) {
+                banque->clients[i].compte[j].dernieres_operations[k].montant = (k + 1) * 100;
+                banque->clients[i].compte[j].dernieres_operations[k].type_operation = AJOUT;
+                banque->clients[i].compte[j].dernieres_operations[k].date_operation.jour = k;
+                banque->clients[i].compte[j].dernieres_operations[k].date_operation.mois = k;
+                banque->clients[i].compte[j].dernieres_operations[k].date_operation.annee = 2023;
+                printf("banque->clients[%d].compte[%d].dernieres_operations[%d].montant = %d\n", i, j, k, banque->clients[i].compte[j].dernieres_operations[k].montant);
+            }
+        }
+    }
+
+}
+
+
 
 
 int main(int argc, char *argv[])
 {
+     banque_t mabanque;
+       initializeBanque(&mabanque);
+
      int sockfd, newsockfd, portno;
      socklen_t clilen;
      char buffer[256];
@@ -85,22 +169,7 @@ int main(int argc, char *argv[])
          fprintf(stderr,"ERROR, no port provided\n");
          exit(1);
      }
-     for( int i = 0; i<10; i++){
-   tab_compte[i].solde = i*10;
-   tab_compte[i].id_compte = i;
-
-   for(int j = 0; j<5; j++){
-      tab_client[j].identifiant = j*3;
-      tab_client[j].password = "abc" + j;
-      tab_client[j].nb_comptes = 2;
-      tab_client[j].id_compte[0] = j;
-      tab_client[j].id_compte[1] = j+1;
-      
-
-
-   }
-   
-}
+    
 
 
      // create a socket
@@ -159,8 +228,7 @@ int main(int argc, char *argv[])
 
 
      // This send() function sends the 13 bytes of the string to the new socket
-       send(newsockfd, "Hello, world!\n", 13, 0);
-         //bzero(buffer,256);
+       send(newsockfd, "Veuillez entrer votre Identifiant client!\n", 13, 0);
 
      
 
@@ -170,9 +238,25 @@ int main(int argc, char *argv[])
      bzero(buffer,256);
       //Lecture de l'identifiant du client
      n = read(newsockfd,buffer,255);
+      printf("buffer = %s\n", buffer);
+
        if (n < 0) perror("ERROR reading from socket");
-       printf("Here is the message: %s\n",buffer);
-      send(newsockfd, "identifiant bien reçu, entrez maintenant votre identifiant\n", 256,0);     
+
+       buffer[strcspn(buffer, "\n")] = '\0';
+
+       
+       if(find_client(atoi(buffer)) == 1){
+       
+
+       printf("Voici l'identifiant  : %s\n",buffer);
+
+      send(newsockfd, "identifiant bien reçu, entrez maintenant votre mot de passe\n", 256,0);
+       }
+       else{
+         send(newsockfd, "identifiant incorrect\n", 256,0);
+
+       }
+        
        bzero(buffer,256);
       }
      
